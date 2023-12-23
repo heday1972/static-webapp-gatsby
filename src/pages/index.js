03c4563e3d9d5ec6647976c2c266e36ec618ece5
Hideaki Takahashi <hideaki.takahashi@protonmail.com>
2023-12-24 02:13:40 +0900

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -82,10 +82,10 @@ const IndexPage = () => (
         style={{ marginBottom: `var(--space-3)` }}
       />
       <h1>
-        Welcome to <b>Gatsby!</b>
+        Welcome to <b>Gatsby! Htakahashi</b>
       </h1>
       <p className={styles.intro}>
-        <b>Example pages:</b>{" "}
+        <b>Example pages: by Hideaki Takahashi</b>{" "}
         {samplePageLinks.map((link, i) => (
           <React.Fragment key={link.url}>
             <Link to={link.url}>{link.text}</Link>
